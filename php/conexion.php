0339eb6e0b0fc32df59e7fc4733e8860204ceb54
mikiweys <dilanfantas@gmail.com>
2023-04-12 18:18:40 -0500

    j

--- php/conexion.php
+++ php/conexion.php
@@ -1,5 +1,5 @@
 <?php 
-$link = 'mysql:host=localhost;dbname=yt_colores';
+$link = 'mysql:host=localhost;dbname=noname';
 $usuario = 'root';
 $pass = '';
 
@@ -7,11 +7,6 @@ try{
 
     $pdo = new PDO($link,$usuario,$pass);
 
-
-//     foreach($pdo->query('SELECT * FROM `colores`') as $fila) {
-//         print_r($fila);
-//     }
-//
 }  
 catch (PDOException $e) {
     print "¡Error!: " . $e->getMessage() . "<br/>";
