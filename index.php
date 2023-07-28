759fbc0f11fb684aa7b4f18c18cc9339f677f44c
mikiweys <114179857+mikiweys@users.noreply.github.com>
2023-07-28 13:41:08 -0500

    SIUUUUUUUUUU

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
+<?php
+include('controller/conexion.php')
+?>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
