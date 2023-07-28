f8f81ed41a4c2cf5d633e6d738780b8f3c371d4a
mikiweys <114179857+mikiweys@users.noreply.github.com>
2023-07-28 14:26:25 -0500

    WAOSS

---
+++ controller/header.php
@@ -0,0 +1,53 @@
+<header>
+<nav class="navbar navbar-expand-xl navbar-light fixed-top bg-white text-center align-items-center shadow">
+  <div class="container-fluid">
+        <a class="navbar-brand me-5 pe-5" href="index.html">
+          <img src="IMG/todo/logo.png" alt="" class="img-fluid " width="100">
+        </a>
+    <form class="d-flex">
+        <input class="form-control me-2 " type="search" placeholder="Busca tu producto" id="search"/>
+        <button class="btn bordenaranja naranja bhovernaranja" type="submit">BUSCAR</button>
+    </form>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse container text-center justify-content-end lg-justify-content-center" id="navbarSupportedContent">
+      <ul class="navbar-nav px-5 mx-5 ">
+        <li class="  col-3 align-self-center text-center">
+            <a class="nav-link  hovernaranja" href="view/nosotros.html">
+                <i class="bi bi-people"></i>
+                <span class="off ">Nosotros</span>
+            </a>
+        </li>
+        <li class="  col-3 align-self-center text-center">
+            <a class="nav-link  hovernaranja" href="view/contacto.html">
+                <i class="bi bi-telephone"></i>
+                <span class="off">Contacto</span> 
+            </a>
+        </li>
+        <li class=" mx-2 col-3 align-self-center text-center">
+          <a class="nav-link hovernaranja" href="view/productos.html">
+            <i class="bi bi-shop"></i>
+              <span class="off">Productos</span> 
+          </a>
+      </li>
+
+        <li class=" dropdown  col-3 align-self-center text-center">
+            <a class="nav-link dropdown-toggle hovernaranja" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <i class="bi bi-person-circle  naranja"></i>
+              <span class="off">Mi cuenta</span>  
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="view/login.html">Ingresa</a></li>
+              <li><hr class="dropdown-divider"></li>
+              <li><a class="dropdown-item" href="view/login.html">Tu perfil</a></li>
+              <li><a class="dropdown-item" href="view/login.html">Tu carrito</a></li>
+              <li><hr class="dropdown-divider"></li>
+              <li><a class="dropdown-item" href="#"><i class="bi bi-box-arrow-right"></i> Cerrar sesion </a></li>
+            </ul> 
+          </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</header>
