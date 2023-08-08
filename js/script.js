73c7900a332642241d523fa49c6cd8495fbb9277
mikiweys <114179857+mikiweys@users.noreply.github.com>
2023-08-08 17:01:13 -0500

    javasc

--- js/script.js
+++ js/script.js
@@ -19,38 +19,34 @@ if (toastTrigger) {
 
 /* TEMA OSCURO */
 var body = document.querySelector('body');
-var itoggle = document.getElementById('itema');
-var btoggle = document.getElementById('btntema');
+var itoggle = document.getElementById('itoggle');
+var btndark = document.getElementById('btndark');
+var btnlight = document.getElementById('btnlight');
+var navbar = document.getElementById('navbar');
+var logo = document.getElementById('logo');
 
-var navbar = document.getElementById('navbar')
-var logo = document.getElementById('logo')
-/* RESPONSIVE */
-var brtoggle = document.getElementById('btnrtema');
-var irtoggle = document.getElementById('irtema');
-var logor = document.getElementById('logor')
-
-/* FUNCION */
-    function tema(){
-      if (body.getAttribute("data-bs-theme") == "light") {
-        body.setAttribute("data-bs-theme", "dark");
-        itoggle.setAttribute("class"," bi bi-sun-fill");
-        btoggle.setAttribute("class","btn text-white");
-        navbar.setAttribute("class","navbar navbar-expand-xl fixed-top navbar-light bg-dark text-center align-items-center shadow");
-        logo.setAttribute("src","IMG/todo/logodark.png");
-        /* responsive */
-        irtoggle.setAttribute("class"," bi bi-sun-fill");
-        brtoggle.setAttribute("class","btn text-white");
-        logor.setAttribute("src","IMG/todo/logodark.png");
-      }
-      else {
-        body.setAttribute("data-bs-theme", "light");
-        itoggle.setAttribute("class"," bi bi-moon-fill");
-        btoggle.setAttribute("class","btn");
-        navbar.setAttribute("class","navbar navbar-expand-xl fixed-top navbar-light bg-light text-center align-items-center shadow");
-        logo.setAttribute("src","IMG/todo/logo.png");
-        /* RESPONSIVE */
-        irtoggle.setAttribute("class"," bi bi-moon-fill");
-        brtoggle.setAttribute("class","btn");
-        logor.setAttribute("src","IMG/todo/logo.png");
-      }
-  }
+    function dark(){
+      body.setAttribute("data-bs-theme", "dark");
+      btndark.setAttribute("class","btn text-white");
+      btnlight.setAttribute("class","btn text-white");
+      itoggle.setAttribute("class","bi bi-moon-fill text-white");
+      navbar.setAttribute("class","navbar navbar-expand-xl fixed-top navbar-dark bg-dark text-center align-items-center shadow text-white");
+      logo.setAttribute("src","IMG/todo/logodark.png");
+      /* RESPONSIVE */
+    }
+    function light(){
+      body.setAttribute("data-bs-theme", "light");
+      btnlight.setAttribute("class","btn");
+      btndark.setAttribute("class","btn");
+      itoggle.setAttribute("class","bi bi-sun-fill");
+      navbar.setAttribute("class","navbar navbar-expand-xl fixed-top navbar-light bg-white text-center align-items-center shadow");
+      logo.setAttribute("src","IMG/todo/logo.png");
+      /* RESPONSIVE */
+    }
+if (body.getAttribute('data-bs-theme')== "light"){
+  light()
+}
+else {
+  dark()
+}
+        
