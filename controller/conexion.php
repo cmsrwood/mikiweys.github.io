e066d34a3c2e7bc484f79af3a269c86ef0716cbb
mikiweys <114179857+mikiweys@users.noreply.github.com>
2023-08-09 16:10:40 -0500

    I TRY TO HIDE IT BUT I KNOW YOU KNOW ME

--- controller/conexion.php
+++ controller/conexion.php
@@ -2,14 +2,10 @@
 $link = 'mysql:host=localhost;dbname=mikiweys';
 $usuario = 'root';
 $pass = '';
-
 try{
-
     $pdo = new PDO($link,$usuario,$pass);
-
 }  
 catch (PDOException $e) {
     print "¡Error!: " . $e->getMessage() . "<br/>";
     die();
 }
-
