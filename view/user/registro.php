21db6e8ec0f59179a53f7324e0c7285603299ca0
mikiweys <114179857+mikiweys@users.noreply.github.com>
2023-08-26 20:10:42 -0500

    MVC

--- view/user/registro.php
+++ view/user/registro.php
@@ -19,13 +19,13 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" >
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
-    <link rel="stylesheet" href="../../../css/styles.css">
+    <link rel="stylesheet" href="../../css/styles.css">
 
     <!-- LOGO ICON -->
-    <link rel="sahortcut icon" href="../../../IMG/todo/logo2.ico">
-    <link rel="icon" sizes="192x192" href="../../../IMG/todo/logo2.ico">
-    <link rel="favicon" href="../../../IMG/todo/logo2.ico">
-    <link rel="apple-touch-icon" href="../../../IMG/todo/logo2.ico">
+    <link rel="sahortcut icon" href="../../IMG/todo/logo2.ico">
+    <link rel="icon" sizes="192x192" href="../../IMG/todo/logo2.ico">
+    <link rel="favicon" href="../../IMG/todo/logo2.ico">
+    <link rel="apple-touch-icon" href="../../IMG/todo/logo2.ico">
     <!-- META  -->
     <meta name="description" content="Panadería Mikiweys">
 </head>
@@ -40,7 +40,7 @@
     <div class="col p-3 simoneta ">
       <div class="row ">
         <div class="col-12"><h1 class="my-4  fs-2">Regístrate</h1></div>
-        <?php include('../../backend/registrar.php');
+        <?php include('../../controller/registrar.php');
       if (isset($_SESSION['mailr'])) {
         echo '<div class="alert alert-danger text-center my-3">' . $_SESSION['mailr'] . '</div>';
         unset($_SESSION['mailr']); 
@@ -107,8 +107,8 @@
     <!-- o -->
     <div class="col-12 col-sm-6 text-center">
       <div class="row text-center align-items-center text-center justify-content-center">
-        <a class="my-5 col-12 pt-5" href="../../../index.html">
-          <img src="../../../IMG/todo/logo.png" alt="" class="img-fluid w-50">
+        <a class="my-5 col-12 pt-5" href="../../index.php">
+          <img src="../../IMG/todo/logo.png" alt="" class="img-fluid w-50">
         </a>
           <div class="facebook my-3">
             <button class="btn btn-primary w-100"><i class="bi bi-facebook pe-1"></i> Ingresa con facebook</button>
