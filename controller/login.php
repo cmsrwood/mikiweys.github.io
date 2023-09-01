7b33ef449a6de7333264e0a97036010b0a0753cb
mikiweys <114179857+mikiweys@users.noreply.github.com>
2023-09-01 15:09:41 -0500

    uy

--- controller/login.php
+++ controller/login.php
@@ -3,25 +3,25 @@
 session_start();
  
 // Conexión a la base de datos
-$db = mysqli_connect('localhost', 'my_user', 'my_password', 'my_db');
- 
+$db = mysqli_connect('localhost', 'root', '', 'mikiweys');
 $errors = [];
+
 // Si se ha enviado el formulario
-if (isset($_POST['login_button'])) {
-  $username = mysqli_real_escape_string($db, $_POST['username']);
-  $password = mysqli_real_escape_string($db, $_POST['password']);
+if (isset($_POST['iniciar'])) {
+  $mail = mysqli_real_escape_string($db, $_POST['mail']);
+  $pass = mysqli_real_escape_string($db, $_POST['pass']);
  
   // Comprobar si el nombre de usuario es válido
-  $query = "SELECT * FROM users WHERE username='$username'";
+  $query = "SELECT * FROM usuarios WHERE email_user='$mail'";
   $results = mysqli_query($db, $query);
  
   if (mysqli_num_rows($results) == 1) {
     // Nombre de usuario válido, verificar contraseña
     $row = mysqli_fetch_assoc($results);
-    if (password_verify($password, $row['password'])) {
+    if (password_verify($pass, $row['pass'])) {
       // Inicio de sesión válido
-      $_SESSION['username'] = $username;
-      header('location: home.phtml');
+      $_SESSION['username'] = $mail;
+      header('location: ../index.php');
     } else {
       // Contraseña inválida
       $errors[] = "Nombre de usuario/contraseña inválidos";
