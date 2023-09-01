9534ab5d39fa9707f302f077e796d30e42031719
mikiweys <114179857+mikiweys@users.noreply.github.com>
2023-09-01 16:44:13 -0500

    a

--- controller/login.php
+++ controller/login.php
@@ -2,28 +2,27 @@
     session_start();
 //conexion
     include('conexion.php');
+
 //inicio por
     if(isset($_SESSION['rol'])){
-        include('../view/user/login.php');
-
         if($_POST['mail']!='' && $_POST['pass']!=''){
             $mensaje='';
             $dni = $_POST['mail'];
             $clave = $_POST['pass'];
-            $rs = $mysqli->query("SELECT * FROM /*tabla*/", MYSQLI_USE_RESULT);
+            $rs = $mysqli->query("SELECT * FROM usuarios", MYSQLI_USE_RESULT);
             $fila = mysqli_fetch_row($rs);
             switch ($_SESSION['rol']){
                 case '1':
                     //administrador
-                    echo 'view/admin/administrador.php';
+                    echo '../view/admin/administrador.php';
                     break;
                 case '2':
                     //empleado
-                    echo 'view/empleado/empleado.php';
+                    echo '../view/empleado/empleado.php';
                     break;
                 case '3':
                     //clientes
-                    echo 'view/cliente/cliente.php';
+                    echo '../view/cliente/cliente.php';
                     break;
         }
         }else{
