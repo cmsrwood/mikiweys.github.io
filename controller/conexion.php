957292b6d14eff52e15302d321fd7b96bb761d1f
mikiweys <114179857+mikiweys@users.noreply.github.com>
2023-08-31 22:49:41 -0500

    session start

--- controller/conexion.php
+++ controller/conexion.php
@@ -1,5 +1,7 @@
+
 <?php
+session_start();
 $PDO = new PDO("mysql:host=localhost;dbname=mikiweys", 'root', '');
-// set the PDO error mode to exception
+// 
 $PDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
