b4bcf13187fd18f77fc15a1bf5549a3596afae61
mikiweys <114179857+mikiweys@users.noreply.github.com>
2023-09-05 20:45:31 -0500

    ok

--- model/insert_producto.php
+++ model/insert_producto.php
@@ -1,3 +1,4 @@
 <?php 
-$insert_producto = "INSERT INTO inventario(imagen,producto,preuni,cat,cantidad) values('$imagen','$producto','$preuni','$cat','$cant')";
+$query = "INSERT INTO inventario(imagen,producto,preuni,cat,cantidad) values('$imagen','$producto','$preuni','$cat','$cant')";
+
 ?>
