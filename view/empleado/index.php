6d7d7b7053587cd895cd4fe23557b44442055cc1
superespecial <140010001+superespecial@users.noreply.github.com>
2023-09-27 14:37:08 -0500

    a

--- view/empleado/index.php
+++ view/empleado/index.php
@@ -4,8 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Empleado | MIKIWEY'S APP</title>
+    <?php  include ('../include/boostrap.php')?>
 </head>
 <body>
-    
+     <div class="container">
+        <h1> ¡ Hola Jose Rafael !
+        </h1>
+
+
+
+
+     </div>
 </body>
 </html>
