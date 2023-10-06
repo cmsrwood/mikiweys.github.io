f9a5d31cdeaa5764be42beed4828961fd598f28d
mikiweys <114179857+mikiweys@users.noreply.github.com>
2023-10-06 16:55:29 -0500

    a

---
+++ view/admin/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio | MIKIWEY'S APP</title>
+</head>
+<body>
+    
+</body>
+</html>
