156c0b0bc6a5e9477c87a84a74b1bbb8c0ef65af
mikiweys <dilanfantas@gmail.com>
2023-10-10 03:49:22 -0500

    a

--- index.php
+++ index.php
@@ -452,6 +452,7 @@
           </div>
         </main>
 <!-- TOAST-->
+
 <div class="toast-container position-fixed bottom-0 end-0 p-3">
   <div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
     <div class="toast-header shadow bg-white align-items-center">
