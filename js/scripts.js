3d3d134075272be335ef33a0b5ddbfe37f108a2e
mikiweys <114179857+mikiweys@users.noreply.github.com>
2023-10-18 23:58:21 -0500

    a

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,3 @@
-/* POPOVER */
-const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
-const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
-
-/* TOOLTIP */
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
-
-/* TOAST */
-const toastTrigger = document.getElementById('liveToastBtn')
-const toastLiveExample = document.getElementById('liveToast')
-
-if (toastTrigger) {
-  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
-  toastTrigger.addEventListener('click', () => {
-    toastBootstrap.show()
-  })
-}
-
 /* TEMA OSCURO */
 var body = document.querySelector('body');
 var itoggle = document.getElementById('itoggle');
