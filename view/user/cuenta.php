9a59edd2620536dcab0fb45c88a721a38589fa6c
Sailast <140013880+Sailast@users.noreply.github.com>
2023-10-19 16:28:31 -0500

    cambio de una parte cuenta

--- view/user/cuenta.php
+++ view/user/cuenta.php
@@ -50,7 +50,9 @@
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        ...
+        <form action="#" method="POST" enctype="multipart/form-data">
+        <input type="file" name="imagen">
+        </form>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cerrar</button>
