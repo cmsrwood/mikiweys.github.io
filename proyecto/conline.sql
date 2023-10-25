46d7661e7f98796ae6692411245b929376692448
mikiweys <114179857+mikiweys@users.noreply.github.com>
2023-10-24 23:15:39 -0500

    bd

---
+++ proyecto/conline.sql
@@ -0,0 +1,186 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 25-10-2023 a las 06:15:21
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `conline`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `notas`
+--
+
+CREATE TABLE `notas` (
+  `id_notas` int(11) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `apellido` varchar(255) NOT NULL,
+  `nombre_doce` varchar(255) NOT NULL,
+  `curso` varchar(255) NOT NULL,
+  `fecha` date NOT NULL,
+  `nota` decimal(5,2) NOT NULL,
+  `notaa` decimal(5,2) NOT NULL,
+  `notas` decimal(5,2) NOT NULL,
+  `notass` decimal(5,2) NOT NULL,
+  `observaciones` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `persona`
+--
+
+CREATE TABLE `persona` (
+  `id_persona` int(11) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `apellido` varchar(255) NOT NULL,
+  `curso` varchar(255) NOT NULL,
+  `fecha` date NOT NULL,
+  `asistio` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `persona`
+--
+
+INSERT INTO `persona` (`id_persona`, `nombre`, `apellido`, `curso`, `fecha`, `asistio`) VALUES
+(2, 'Laura Valentina', 'Romero Diaz', '1102', '2007-07-02', 0),
+(3, 'Laura Valentina', 'Romero Diaz', '1102', '2007-07-02', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `registro`
+--
+
+CREATE TABLE `registro` (
+  `id_registro` int(11) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `apellido` varchar(255) NOT NULL,
+  `correo` varchar(255) NOT NULL,
+  `contrasena` varchar(255) NOT NULL,
+  `fecha_nac` date NOT NULL,
+  `num_doc` varchar(20) NOT NULL,
+  `rol_id` int(11) DEFAULT NULL,
+  `telefono` varchar(15) NOT NULL,
+  `tip_doc_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `registro`
+--
+
+INSERT INTO `registro` (`id_registro`, `nombre`, `apellido`, `correo`, `contrasena`, `fecha_nac`, `num_doc`, `rol_id`, `telefono`, `tip_doc_id`) VALUES
+(5, 'Laura Valentina', 'Ramirez Diaz', 'laura@gmail.com', 'contrasena', '2007-05-04', '10558685875', 0, '3156859575', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `roll`
+--
+
+CREATE TABLE `roll` (
+  `id_rol` int(11) NOT NULL,
+  `roll` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tip_doc`
+--
+
+CREATE TABLE `tip_doc` (
+  `id_tipdoc` int(11) NOT NULL,
+  `tip_doc` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `notas`
+--
+ALTER TABLE `notas`
+  ADD PRIMARY KEY (`id_notas`);
+
+--
+-- Indices de la tabla `persona`
+--
+ALTER TABLE `persona`
+  ADD PRIMARY KEY (`id_persona`);
+
+--
+-- Indices de la tabla `registro`
+--
+ALTER TABLE `registro`
+  ADD PRIMARY KEY (`id_registro`);
+
+--
+-- Indices de la tabla `roll`
+--
+ALTER TABLE `roll`
+  ADD PRIMARY KEY (`id_rol`);
+
+--
+-- Indices de la tabla `tip_doc`
+--
+ALTER TABLE `tip_doc`
+  ADD PRIMARY KEY (`id_tipdoc`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `notas`
+--
+ALTER TABLE `notas`
+  MODIFY `id_notas` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `persona`
+--
+ALTER TABLE `persona`
+  MODIFY `id_persona` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `registro`
+--
+ALTER TABLE `registro`
+  MODIFY `id_registro` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `roll`
+--
+ALTER TABLE `roll`
+  MODIFY `id_rol` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tip_doc`
+--
+ALTER TABLE `tip_doc`
+  MODIFY `id_tipdoc` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
