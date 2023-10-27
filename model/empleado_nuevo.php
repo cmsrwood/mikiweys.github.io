f93731f0716c015bd3553025b270b38a8983d5c9
mikiweys <114179857+mikiweys@users.noreply.github.com>
2023-10-27 14:13:45 -0500

    si se ve

--- model/empleado_nuevo.php
+++ model/empleado_nuevo.php
@@ -1,7 +1,7 @@
 <?php 
 $query = "INSERT INTO empleados(nom,apel,fecing,cont,tel,sal,hor,email) values('$nombre','$apellido','$fecing','$contrato','$tel','$salario','$hor','$mail')
-SELECT * FROM `usuarios` WHERE email_user= '$mail'
-
+SELECT * FROM usuarios WHERE email_user= '$mail'
+UPDATE usuarios SET rol_user = 2 WHERE email_user = '$mail';
 ";
 $resultado = mysqli_query($db,$query);
 ?>
