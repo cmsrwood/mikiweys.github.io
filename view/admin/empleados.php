1d7d406aa9d5ea81929422bcf9eedd0e90e7a08c
mikiweys <114179857+mikiweys@users.noreply.github.com>
2023-10-27 13:55:04 -0500

    empleados tabla arreglada

--- view/admin/empleados.php
+++ view/admin/empleados.php
@@ -64,10 +64,12 @@
         <th><?php echo $empleado['id_emp']; ?></th>
         <td><?php echo $empleado['nom']; ?></td>
         <td><?php echo $empleado['apel']; ?></td>
+        <td><?php echo $empleado['email']; ?></td>
         <td><?php echo $empleado['fecing']; ?></td>
         <td><?php echo $empleado['cont']; ?></td>
         <td><?php echo $empleado['tel']; ?></td>
         <td><?php echo $empleado['sal']; ?></td>
+        <td><?php echo $empleado['hor']; ?></td>
         <td><a href="empleado_modificar.php?id=<?=$empleado['id_emp']?>" type="button" class="btn btn-warning"><i class="bi bi-pencil-square"></i></a></td>
         <td><a href="../../controller/empleado_eliminar.php?id=<?=$empleado['id_emp']?>" type="button" class="btn btn-danger"><i class="bi bi-trash"></i></a></td>
       </tr>
