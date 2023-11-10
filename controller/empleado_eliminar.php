463d1fb03e17301c7adacacf733a5ad78bd87f11
mikiweys <114179857+mikiweys@users.noreply.github.com>
2023-11-10 13:46:09 -0500

    SO CALL OUT MY NAMEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE

--- controller/empleado_eliminar.php
+++ controller/empleado_eliminar.php
@@ -1,8 +1,13 @@
 <?php
 include('login.php');
     $id =$_GET['id'];
+    $select = "SELECT * FROM empleados WHERE id_emp='$id'";
+    $resultado = mysqli_query($db,$select); 
+    $empleado = mysqli_fetch_assoc($resultado);
+    $mail=$empleado['email'];
     $eliminar = "DELETE FROM empleados WHERE id_emp='$id'";
-    $resultado = mysqli_query($db,$eliminar); 
+    $resultado2 = mysqli_query($db,$eliminar); 
+
     /* ELIMINADO */
     if ($resultado){
     $sql = "UPDATE usuarios SET rol_user = '3' WHERE email_user ='$mail'";
